61e5bbcd1fab496a4acdb28bb266c6b5743131da
Varun Bansal <varunbansal@Varuns-MacBook-Air.local>
2023-10-14 12:24:07 +0530

    WR2-1

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -30,8 +30,6 @@ class Reward:
                         bonus = 1
                     if(steering_angle==self.previous_steering_angle):
                         steering_bonus=10*min(20,self.count)
-                if(not params["is_left_of_center"]):
-                        corner_reward=40
                         
             elif (waypoint > 27 and waypoint < 45) or (waypoint >179 and waypoint< 189 ) or (waypoint >81 and waypoint <88):
                 if steering_angle == 0 or steering_angle == 10:
@@ -50,9 +48,6 @@ class Reward:
                         bonus = 2
                     if(steering_angle==self.previous_steering_angle):
                         steering_bonus=10*min(20,self.count)
-                if(not params["is_left_of_center"]):
-                        corner_reward=40
-                
             elif (waypoint >55 and waypoint< 65 ):
                 if  steering_angle < 0:
                     if speed  > 3 :
@@ -83,7 +78,7 @@ class Reward:
                         bonus = 14
                     if(steering_angle==self.previous_steering_angle):
                         steering_bonus=10*min(20,self.count)
-                    if(not params["is_left_of_center"]):
+                    if(params["is_left_of_center"]):
                         corner_reward=100
             else:
                 if  steering_angle >= 0:
